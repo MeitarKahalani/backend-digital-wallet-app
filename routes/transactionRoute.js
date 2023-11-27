04903d3c6923dd7b306aa5a1c0c7d650d6cdd67a
MeitarKahalani <Meitark95@gmail.com>
2023-11-27 17:22:13 +0200

    extend classes and services

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -1,17 +1,22 @@
 const router = require("express").Router();
-import TransactionService from './services/transactionService';
+const TransactionService = require('./services/transactionService');
 
 const transactionService = new TransactionService();
 
 // Transaction Service Endpoints
-router.post('/', (req, res) => {
-    // Initiate a transaction
-    // Example: transactionService.initiateTransaction(req.body.senderId, req.body.receiverId, req.body.amount).then(transaction => res.json(transaction));
-  });
-  
+router.post('/transactions', async (req, res) => {
+    try {
+        const { senderId, receiverId, amount } = req.body;
+        const newTransaction = await transactionService.initiateTransaction(senderId, receiverId, amount);
+        res.json(newTransaction);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to initiate transaction' });
+    }
+});
+
 router.patch('/:transactionId', (req, res) => {
     // Process a transaction
     // Example: transactionService.processTransaction(req.params.transactionId).then(transaction => res.json(transaction));
-  });
+});
 
-  export default router;
+module.exports = router;
