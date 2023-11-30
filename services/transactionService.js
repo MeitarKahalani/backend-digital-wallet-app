19849e29807525fc0f647a4a5a36fd920ba6edb6
MeitarKahalani <Meitark95@gmail.com>
2023-11-30 13:44:40 +0200

    full system

--- services/transactionService.js
+++ services/transactionService.js
@@ -1,19 +1,21 @@
 const { MongoClient, ObjectId } = require('mongodb');
 const UserService = require('../services/userService');
+const NotificationService = require('../services/notificationService');
 
 class TransactionService {
   constructor() {
     this.client = new MongoClient('mongodb://localhost:27017');
     this.db = null;
     this.collection = null;
-    this.connect();
     this.userService = new UserService();
+    this.notificationService = new NotificationService();
+    this.connect();
   }
 
+  // Establish a connection to the MongoDB database
   async connect() {
     try {
       await this.client.connect();
-      // console.log("connected to MongoDB")
       this.db = this.client.db('digitalWalletDB');
       this.collection = this.db.collection('transactions');
     } catch (error) {
@@ -22,20 +24,20 @@ class TransactionService {
     }
   }
 
+  // Initiates a transaction and inserts it into the collection
   async initiateTransaction(senderId, receiverId, amount) {
     try {
       const transaction = {
         senderId: parseInt(senderId, 10),
         receiverId: parseInt(receiverId, 10),
         amount: amount,
-        status: 'pending' // various statuses (pending, completed, rejected)
+        status: 'pending', // various statuses (pending, completed, rejected)
+        receiverDecision: null // Keeps track of receiver's decision (accept/deny)
       };
-      // Initiate the transaction
-      const newTransaction = await this.collection.insertOne(transaction);
 
-         // Notify receiver about the pending transaction
-      await this.notificationService.sendNotification(receiverId, `You have a pending transaction. Accept or Deny?`);
-      
+      const newTransaction = await this.collection.insertOne(transaction);
+      // Notifies the receiver about the pending transaction
+      this.notificationService.sendNotification(receiverId, `You have a pending transaction. Accept or Deny?`);
       return newTransaction.insertedId;
     } catch (error) {
       console.error('Error initiating transaction:', error.message);
@@ -43,36 +45,11 @@ class TransactionService {
     }
   }
 
-  async processTransaction(transactionId) {
-    try {
-      let updateStatus = '';
-
-      if (receiverDecision === 'accept') {
-        updateStatus = 'completed';
-      } else if (receiverDecision === 'deny') {
-        updateStatus = 'rejected';
-      } else {
-        throw new Error('Invalid receiver decision');
-      }
-      await this.collection.updateOne(
-        { _id: transactionId },
-        { $set: { status: 'completed' } }
-      );
-      const transaction = await this.getTransactionById(transactionId);
-      if (transaction.status === 'completed') {
-        const receiverId = transaction.receiverId;
-        const amount = transaction.amount;
-        await this.userService.notifyUser(receiverId, `You've received ${amount} dollars.`);
-      }
-    } catch (error) {
-      console.error('Error processing transaction:', error.message);
-      throw new Error('Failed to process transaction');
-    }
-  }
-
+  // Retrieves transaction details from Mongodb by transaction ID
   async getTransactionById(transactionId) {
     try {
-      const transactionDetails = await this.collection.findOne({ _id: transactionId });
+      const transactionIdAsObject = new ObjectId(transactionId);
+      const transactionDetails = await this.collection.findOne({ _id: transactionIdAsObject });
       return transactionDetails;
     } catch (error) {
       console.error('Error fetching transaction:', error.message);
@@ -80,12 +57,61 @@ class TransactionService {
     }
   }
 
+  // Processes a transaction by ID based on receiver's decision
+  async processTransaction(transactionId) {
+    try {
+      const transaction = await this.getTransactionById(transactionId);
+      const transactionIdAsObject = new ObjectId(transactionId);
+
+      if (transaction.status === 'pending') {
+        const receiverDecision = transaction.receiverDecision;
+
+        let updateStatus = '';
+        if (receiverDecision === 'accept') {
+          // Updates transaction status to 'completed' if receiver accepts
+          updateStatus = 'completed';
+          // Notifies sender and receiver about the transaction completion
+          this.notificationService.sendNotification(transaction.senderId, 'Receiver accepted the transaction.');
+          this.notificationService.sendNotification(receiverId, `You've received ${amount} dollars.`);
+        } else if (receiverDecision === 'deny') {
+          // Updates transaction status to 'rejected' if receiver denies
+          updateStatus = 'rejected';
+          // Notifies sender about the transaction denial
+          this.notificationService.sendNotification(transaction.senderId, 'Receiver denied the transaction.');
+        } else {
+          throw new Error('Invalid receiver decision');
+        }
+
+        // Updates the status of the transaction in the database
+        await this.collection.updateOne(
+          { _id: transactionIdAsObject },
+          { $set: { status: updateStatus } }
+        );
+
+        if (updateStatus === 'completed') {
+          // Retrieves sender and receiver details to update balances
+          const { senderId, receiverId, amount } = transaction;
+          // Updating sender and receiver balances
+          await this.updateSenderAndReceiverBalances(senderId, receiverId, amount, transactionIdAsObject);
+          // Notifies sender and receiver about the completion of the transaction
+          this.notificationService.sendNotification(senderId, 'Transaction completed successfully.');
+          this.notificationService.sendNotification(receiverId, 'Transaction completed successfully.');
+        }
+      } else {
+        throw new Error('Transaction is already processed');
+      }
+    } catch (error) {
+      console.error('Error processing transaction:', error.message);
+      throw new Error('Failed to process transaction');
+    }
+  }
+
+  // Updates sender and receiver balances after verifying sender has sufficient balance
   async updateSenderAndReceiverBalances(senderId, receiverId, amount, newTransactionId) {
     try {
       const sender = await this.userService.getUserById(senderId);
       const receiver = await this.userService.getUserById(receiverId);
 
-      // console.log(sender,receiver);
       if (!sender || !receiver) {
         throw new Error('Sender or Receiver not found');
       }
@@ -94,6 +120,7 @@ class TransactionService {
         throw new Error('Insufficient balance in the sender account');
       }
 
+      // Define transaction details for sender and receiver
       const senderTransaction = {
         transactionId: newTransactionId, // Generate a transaction ID
         type: 'debit',
@@ -108,6 +135,7 @@ class TransactionService {
         date: new Date().toLocaleString() // Date of the transaction
       };
 
+      // Update sender and receiver balances in the database
       const updatedSender = await this.userService.updateUserBalance(
         sender.userid,
         sender.wallet.balance - amount,
@@ -124,6 +152,32 @@ class TransactionService {
     } catch (error) {
       console.error('Error updating balances:', error.message);
       throw new Error('Failed to update balances');
+    }
+  }
+
+  // Handles receiver's response (accept/deny) for a pending transaction 
+  async handleReceiverResponse(transactionId, receiverResponse) {
+    try {
+      const transaction = await this.getTransactionById(transactionId);
+      const transactionIdAsObject = new ObjectId(transactionId);
+      if (!transaction || transaction.status !== 'pending') {
+        throw new Error('Invalid transaction or already processed');
+      }
+
+      if (receiverResponse === 'accept' || receiverResponse === 'deny') {
+        await this.collection.updateOne(
+          { _id: transactionIdAsObject },
+          { $set: { receiverDecision: receiverResponse } }
+        );
+
+        await this.processTransaction(transactionId);
+        return `Transaction ${receiverResponse === 'accept' ? 'accepted' : 'denied'} successfully`;
+      } else {
+        throw new Error('Invalid receiver response');
+      }
+    } catch (error) {
+      console.error('Error handling receiver response:', error.message);
+      throw new Error('Failed to handle receiver response');
     }
   }
 }
